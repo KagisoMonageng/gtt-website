10eafecab7b38b37700d7956e6980c36aacad204
KagisoMonageng <63680048+KagisoMonageng@users.noreply.github.com>
2024-01-08 21:42:42 +0200

    ypdate main

--- site/src/main.ts
+++ site/src/main.ts
@@ -1,10 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { injectSpeedInsights } from "@vercel/speed-insights" 
+import { injectSpeedInsights } from "@vercel/speed-insights"
 
 
 import { AppModule } from './app/app.module';
 
-injectSpeedInsights(null)
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+injectSpeedInsights(null)
