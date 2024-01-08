8374166184cbf60d2b4421a087e3da64c6293508
KagisoMonageng <63680048+KagisoMonageng@users.noreply.github.com>
2024-01-08 21:39:50 +0200

    insights

--- site/src/app/app.component.ts
+++ site/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,5 +12,6 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     initFlowbite();
+   
   }
 }
